332a48db1f904572d0d58ee04e8316bcaea3f1a0
ayushblaze <ayushmoca@gmail.com>
2025-07-21 10:12:59 +0530

    feat(event-bus): create event-bus service to transmit events to other services

---
+++ event-bus/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const axios = require("axios");
+const chalk = require("chalk");
+
+const app = express();
+app.use(express.json());
+
+const events = [];
+
+app.post('/events', async (req, res) => {
+  const event = req.body;
+
+  events.push(event);
+
+  await axios.post('http://localhost:4000/events', event);
+  await axios.post('http://localhost:4001/events', event);
+  await axios.post('http://localhost:4002/events', event);
+  await axios.post('http://localhost:4003/events', event);
+
+  res.send({ status: "OK" });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
+});
+
+app.listen(4005, () => {
+console.log(chalk.bgGreen.white.bold('Server listening on PORT 4005'));
+});
